d3a770836bb5ada8c765011ba6502cc163be15d6
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-01-20 15:26:32 -0500

    Feat: add .env example add update variables

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,10 @@
 import { Server } from "./presentation/Server";
 import { envs } from "./config/envs.plugin";
 import { errorHandler } from "./middlewares/ErrorHandler";
-import { MainRouter } from "./routes/mainRouter";
+import { MainRouter as Router } from "./routes/mainRouter";
 
 const main = async (): Promise<void> => {
-  const server = new Server(
-    envs.SERVER_PORT,
-    MainRouter,
-    "public",
-    errorHandler,
-  );
+  const server = new Server(envs.SERVER_PORT, Router, "public", errorHandler);
 
   server.start();
 };
--- backend/src/config/envs.plugin.ts
+++ backend/src/config/envs.plugin.ts
@@ -4,4 +4,7 @@ import * as env from "env-var";
 export const envs = {
   ENVIRONMENT: env.get("ENVIRONMENT").required().asString(),
   SERVER_PORT: env.get("SERVER_PORT").required().asIntPositive(),
+  AWS_ACCESS_KEY_ID: env.get("AWS_ACCESS_KEY_ID").required().asString(),
+  AWS_SECRET_ACCESS_KEY: env.get("AWS_SECRET_ACCESS_KEY").required().asString(),
+  AWS_REGION: env.get("AWS_REGION").required().asString(),
 };
