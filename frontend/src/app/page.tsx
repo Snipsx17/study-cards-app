0ff7aae76579aff5ae4249e6724705570dc9d2cf
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-01-25 14:23:23 -0500

    Feat: start frontend project

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <header>
+        {/* logo */}
+        <div></div>
+        <nav></nav>
+      </header>
+      <main></main>
+    </>
+  );
+}
