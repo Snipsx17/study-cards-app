34d071de074ac5e2fe3b9966abd368ce672c1df6
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-01-30 16:42:16 -0500

    Refactor: update server props

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,16 @@
-import { Server } from "./presentation/Server";
-import { envs } from "./config/envs.plugin";
-import { errorHandler } from "./middlewares/ErrorHandler";
-import { MainRouter as Router } from "./routes/mainRouter";
+import { Server } from "@/presentation/Server";
+import { envs } from "@/config/envs.plugin";
+import { errorHandler } from "@/presentation/middlewares/ErrorHandler";
+import { MainRouter } from "@/presentation/router/main.router";
 
 const main = async (): Promise<void> => {
-  const server = new Server(envs.SERVER_PORT, Router, "public", errorHandler);
+  const mainRouter = new MainRouter();
+  const server = new Server(
+    envs.SERVER_PORT,
+    mainRouter.apiV1,
+    "public",
+    errorHandler,
+  );
 
   server.start();
 };
