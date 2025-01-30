fed9ee5b57b60c6b8799d516538b172f1f91be2e
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-01-30 16:37:51 -0500

    Feat: create implementation for user datasource and repository

---
+++ backend/src/infrastructure/datasources/aws-user.datasource.impl.ts
@@ -0,0 +1,74 @@
+import { UserDatasource } from "@/domain/datasource/User.datasource";
+import { UserEntity } from "@/domain/entities/User.entity";
+import { prisma } from "../../data/postgres";
+import { envs } from "@/config/envs.plugin";
+
+export class UserDatasourceImpl extends UserDatasource {
+  async userExists(user: UserEntity): Promise<boolean | undefined> {
+    try {
+      const userExists = await prisma.user.findFirst({
+        where: {
+          OR: [{ username: user.username }, { email: user.email }],
+        },
+      });
+
+      if (!userExists) {
+        return false;
+      }
+
+      return !!userExists;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error(error.message);
+      }
+    }
+  }
+
+  getUserById(id: number): Promise<UserEntity | undefined> {
+    throw new Error("Method not implemented.");
+  }
+  async getUserByUsername(username: string): Promise<UserEntity | undefined> {
+    try {
+      const user = await prisma.user.findFirst({
+        where: {
+          username: username,
+        },
+      });
+
+      if (!user) {
+        return undefined;
+      }
+
+      return new UserEntity(new UserEntity(user));
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+    }
+  }
+
+  getUserByEmail(email: string): Promise<UserEntity | undefined> {
+    throw new Error("Method not implemented.");
+  }
+  async createUser(user: UserEntity): Promise<UserEntity | undefined> {
+    try {
+      const newUser = await prisma.user.create({
+        data: {
+          name: user.name,
+          email: user.email,
+          username: user.username,
+          password: user.password,
+          avatar_url: user.avatarUrl || envs.DEFAULT_AVATAR_IMG,
+        },
+      });
+      console.log(newUser);
+      return newUser;
+    } catch (error) {
+      if (error instanceof Error) throw new Error(error.message);
+    }
+  }
+  updateUser(user: UserEntity): Promise<UserEntity | undefined> {
+    throw new Error("Method not implemented.");
+  }
+  deleteUser(id: number): Promise<boolean> {
+    throw new Error("Method not implemented.");
+  }
+}
---
+++ backend/src/infrastructure/repositories/log.repository.impl.ts
@@ -0,0 +1,16 @@
+import { LogEntity, LogLevel, logType } from "../../domain/entities/Log.entity";
+import { LogRepository } from "../../domain/repositories/Log.repository";
+import { AwsLogDatasource } from "../datasources/aws-log.datasource.impl";
+
+export class LogRepositoryImpl implements LogRepository {
+  constructor(private readonly logDataSource: AwsLogDatasource) {}
+  async getLogByLevel(level: LogLevel): Promise<LogEntity[] | undefined> {
+    return await this.logDataSource.getLogByLevel(level);
+  }
+  async getLogByType(type: logType): Promise<LogEntity[] | undefined> {
+    return await this.logDataSource.getLogByType(type);
+  }
+  async saveLog(log: LogEntity): Promise<void> {
+    this.logDataSource.saveLog(log);
+  }
+}
