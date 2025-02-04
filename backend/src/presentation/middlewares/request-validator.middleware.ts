2e8472361a297ff23e2a5c59e08c0ab584c04f7b
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-04 09:50:09 -0500

    Feat: add error log on request validator middleware

--- backend/src/presentation/middlewares/request-validator.middleware.ts
+++ backend/src/presentation/middlewares/request-validator.middleware.ts
@@ -1,10 +1,15 @@
+import { LogEntity, LogLevel, logType } from "@/domain/entities/Log.entity";
 import { UserSchema } from "@/domain/schemas/user.schema";
+import { AwsLogDatasource } from "@/infrastructure/datasources/aws-log.datasource.impl";
+import { LogRepositoryImpl } from "@/infrastructure/repositories/log.repository.impl";
 import { NextFunction, Request, Response } from "express";
 import { z, ZodSchema } from "zod";
 
 export interface RequestWithUserData extends Request {
   userData?: z.infer<typeof UserSchema>;
 }
+
+const errorLog = new LogRepositoryImpl(new AwsLogDatasource());
 
 export class CreateRequestValidator {
   validate(schema: ZodSchema<any>) {
@@ -26,7 +31,17 @@ export class CreateRequestValidator {
         req.userData = req.body;
         next();
       } catch (error) {
-        if (error instanceof Error) next(error);
+        if (error instanceof Error) {
+          errorLog.saveLog(
+            new LogEntity(
+              logType.AUTH,
+              LogLevel.LOW,
+              `User data not valid: ${JSON.stringify(error)}`,
+            ),
+          );
+
+          next(error);
+        }
       }
     };
   }
