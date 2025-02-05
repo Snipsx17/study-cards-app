29eb7b74b28c7b095d166948e6db42c7cd29b8f9
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-05 11:02:46 -0500

    Feat: create jwt adapter

---
+++ backend/src/config/jwt.plugin.ts
@@ -0,0 +1,61 @@
+import { config } from "dotenv";
+config();
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { envs } from "./envs.plugin";
+
+export class tokenJwt {
+  //   createToken = ({ data, exp }: TokenParams): string => {
+  //     const jwtSecret = process.env.JWT_TOKEN_SECRET || "secret";
+  //     const token = jwt.sign(
+  //       {
+  //         data,
+  //       },
+  //       jwtSecret,
+  //       { expiresIn: exp },
+  //     );
+  //     return token;
+  //   };
+
+  static createRefreshToken = (data: any): string => {
+    const refreshTokenSecret = envs.JWT_SECRET;
+    const tokenExpiration = envs.JWT_REFRESH_TOKEN_EXPIRATION || "15d";
+    const token = jwt.sign(
+      {
+        data,
+      },
+      refreshTokenSecret,
+      { expiresIn: tokenExpiration } as jwt.SignOptions,
+    );
+    return token;
+  };
+
+  validateToken = (token: string) => {
+    const secret = process.env.JWT_TOKEN_SECRET || "secret";
+    try {
+      const { data } = jwt.verify(token, secret) as JwtPayload;
+
+      const userID: string = data._id;
+
+      return userID;
+    } catch (error) {
+      throw new Error("Invalid Token");
+    }
+  };
+
+  validateRefreshToken = (token: string) => {
+    const secret = process.env.REFRESH_JWT_TOKEN_SECRET || "secret";
+    try {
+      const { user, iat, exp } = jwt.verify(token, secret) as JwtPayload;
+
+      const userData = {
+        user,
+        iat,
+        exp,
+      };
+
+      return userData;
+    } catch (error) {
+      return null;
+    }
+  };
+}
