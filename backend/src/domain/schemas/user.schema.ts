2771bbef67abb1a1065df37191c205ee21cb335c
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-12 14:18:08 -0500

    Feat: add user schema

--- backend/src/domain/schemas/user.schema.ts
+++ backend/src/domain/schemas/user.schema.ts
@@ -10,12 +10,3 @@ export const UserSchema = z.object({
     .regex(/[A-Za-z]/, "Password must contain at least one letter")
     .regex(/\d/, "Password must contain at least one number"),
 });
-
-export const LoginUserSchema = z.object({
-  username: z.string().max(50, "Username must not exceed 50 characters"),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters long")
-    .regex(/[A-Za-z]/, "Password must contain at least one letter")
-    .regex(/\d/, "Password must contain at least one number"),
-});
