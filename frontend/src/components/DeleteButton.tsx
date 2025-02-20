589753aedf4104546d86b1a51d3bbee5c4227b5d
Snipsx17 <ing.uberthhernandez@gmail.com>
2025-02-19 21:13:15 -0500

    Feat: add delete button component

---
+++ frontend/src/components/DeleteButton.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+
+interface DeleteButtonProps {
+  onDelete: () => void;
+  title: string;
+  description: string;
+  children?: React.ReactNode;
+}
+
+export const DeleteButton: FC<DeleteButtonProps> = ({
+  onDelete,
+  title,
+  description,
+  children,
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className="w-full md:flex-1" size={"lg"}>
+          {children}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+            onClick={onDelete}
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
